8380a63f343a944717099d815a309dd8be980f00
Davis <mad221@musc.edu>
2017-12-28 10:06:50 -0500

    added calendar period functions all seem to be working

---
+++ R/calendar_periods.R
@@ -0,0 +1,112 @@
+#' @title cp add 1 
+#' @param x a signle calendar period ie: 201202
+#' @param n int number of months to add
+#' @author Matthew Davis
+#' @description adds months to a calendar period, hidden helper function
+#' @return a integet calendar period
+#' 
+cp_add_1 = function(x, n){
+  x = as.character(x)
+  y = as.numeric(substr(x, 1,4))
+  m =  as.numeric(substr(x, 5,6))
+  ny = as.character(y + (n+m)%/% 12)
+  nm = as.character((n+m)%%12)
+  if(nm == '0'){
+    nm = '12'
+    ny = as.character(as.numeric(ny) - 1)
+  }
+  if(nchar(nm) == 1){nm = paste('0', nm, sep = '')}
+  output = as.numeric(paste(ny, nm, sep = ''))
+  return(output)
+}
+
+#' @title cp add  
+#' @param x int vector of calendar periods ie: 201202
+#' @param n int number of months to add
+#' @author Matthew Davis
+#' @description adds months to a calendar period, hidden helper function
+#' @return a integer calendar period
+#' @export
+
+cp_add = function(x ,n = 6){
+  unlist(lapply(x, function(x)cp_add_1(x, n)))
+}
+
+#' @title cp to fp   
+#' @param x int vector of calendar periods ie: 201202
+#' @param n int number of months to add
+#' @param rev logical, reverse and go from fiscal period to calendar period 
+#' @author Matthew Davis
+#' @description adds months to a calendar period, hidden helper function
+#' @return a integer calendar periods
+#' @export
+
+cp_to_fp = function(x, n = 6, rev = FALSE){
+  if(!is.null(attr(x, 'fiscal'))){
+    if(attr(x, 'fiscal'))rev = TRUE
+    if(!attr(x, 'fiscal'))rev = FALSE
+  }
+  if(rev){n = -1*n}
+  output = unlist(lapply(x, function(x)cp_add_1(x, n)))
+  attr(output, 'fiscal') = !rev
+  return(output)
+}
+
+#' @title cp_seq 
+#' @param x int calendar period to start ie 201202
+#' @param n int number of months to add in the sequence
+#' @param use_first logical, in cluded x (as the start of the sequence) 
+#' @author Matthew Davis
+#' @description creates a sequence of calendar periods
+#' @return a integer calendar period
+#' @export
+
+cp_seq = function(x, n = 6, use_first = TRUE){
+  y = x
+  n = seq(n) 
+  if(use_first)n = n-1
+  output = unlist(lapply(n, function(x)cp_add_1(y, x)))
+  return(output)
+}
+
+#' @title CP to Calendar 
+#' @param x int calendar period to start ie 201202
+#' @param fp whether the calindar period is fiscal 
+#' @author Matthew Davis
+#' @import timeDate
+#' @description finds holidays and buisness days for a calendar period
+#' @return a vector of business days and holiday/weekends
+#' 
+cp_to_calendar_1 = function(x, fp = FALSE){
+  if(!is.null(attr(x, 'fiscal'))){
+    if(attr(x, 'fiscal'))fp= TRUE
+    if(!attr(x, 'fiscal'))fp = FALSE
+  }
+  if(fp){x =cp_to_fp(x)}
+  x1 = cp_add_1(x,1)
+  x = as.character(x)
+  y = substr(x, 1,4)
+  m = substr(x,5,6) 
+  y1 = substr(x1, 1,4)
+  m1 =  substr(x1, 5,6)
+  start = as.Date(paste(paste(y,m, sep = '-'), 01, sep = '-'))
+  end = as.Date(paste(paste(y1,m1, sep = '-'), 01, sep = '-'))-1
+  time_seq = timeDate::timeSequence(start, end)
+  output = c(holidays_weekends = sum(timeDate::isHoliday(time_seq)), 
+            biz_days = sum(timeDate::isBizday(time_seq)) )
+  return(output)
+}
+
+#' @title CP to Calendar 
+#' @param x int calendar period to start ie 201202
+#' @param fp whether the calindar period is fiscal 
+#' @author Matthew Davis
+#' @description creates a sequence of calendar periods
+#' @return a vector of business days and holiday/weekends
+#' @export
+
+cp_to_calendar = function(x, fp = FALSE){
+  t(sapply(x, function(y)cp_to_calendar_1(y, fp = fp)))
+  }
+  
+  
--- vignettes/my-vignette.R
+++ vignettes/my-vignette.R
@@ -78,3 +78,12 @@ predict(lc, data)
 data = c('aabb', 'aacc', 'aaaa', 'bbaa','bbbb','bbcc')
 preFixes = c('aa', 'bb')
 
+## ----cp functions--------------------------------------------------------
+x = c(201209,201212, 201301)
+cp_add(x,2)
+cp_to_fp(x)
+cp_to_fp(cp_to_fp(x), rev = TRUE)
+cp_seq(201201,6)
+cp_to_calendar(c(201712,201801))
+
+
