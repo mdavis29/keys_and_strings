9160d03aceee643d5d771a5501427b18c9f46f4e
Davis, Matthew <davismat@musc.edu>
2018-01-03 12:07:05 -0500

    updated missing cp to return null when no matches found

--- R/missing_cp.R
+++ R/missing_cp.R
@@ -6,18 +6,21 @@
 #' @description splits a string in to chunks of length chunk_len, returns a matrix
 #' @return a vector of missing calendar periods, or a vector of complete calendar periods from min to max data
 #' @export
-# create the level coder objec
 
-missing_cp = function(data,return_completed = FALSE, verbose = FALSE){
+missing_cp <-function(data,return_completed = FALSE, verbose = FALSE){
+  output = NULL
   max_cp = max(data, na.rm = TRUE)
   min_cp = min(data, na.rm = TRUE)
   new_cp = cp_seq(min_cp, max_cp - min_cp, use_first = TRUE )
   new_cp = new_cp[new_cp <=max_cp & new_cp >=min_cp]
-  output = setdiff(new_cp, data)
-  if(verbose)print(paste('missing', length(output), sep = ':'))
-  if(return_completed){
-    output = unique(c(data, output))
+  diffs = setdiff(new_cp, data)
+  if(length(diffs)>0){
+    output = diffs
+    if(verbose)print(paste('missing', length(output), sep = ':'))
+    if(return_completed){
+      output = unique(c(data, output))
+    }
     output = output[order(output)]
   }
   return(output)  
-  }
+}
